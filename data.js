e85bffa72382e86d756d94d604ddc84b37cef1d7
emito182-droid <emito182@gmail.com>
2026-03-13 22:42:41 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,85 @@
+const db_documentos = {
+    "Memorando Nro. ENAMI-GDM-2026-0059-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-GDM-2026-0059-MEM",
+        "fecha": "Quito, D.M., 11 de marzo de 2026",
+        "para": "<b>Sra. Abg. Gabriela Sibia Jauregui Moran</b><br>Gerente Negocios Mineros",
+        "de": "<b>Mgs. Diego Andrés Rueda Albuja</b><br><small>GERENTE DE GESTIÓN Y DESARROLLO MINERO</small>",
+        "asunto": "Consideración y análisis del Informe Técnico No. 79-DAS-2025 - Visita a los Proyectos de la Empresa SILVERCORP (Diciembre 2025)",
+        "copias": [
+            "Sr. Mgs. Galo Andrés García Medina",
+            "Gerente General, Subrogante",
+            "Sr. Mgs. Lenin Vladimir Ochoa Ochoa",
+            "Subgerente Jurídico",
+            "Sr. Ing. Marlon Oswaldo Lara Mier",
+            "Director Ambiental y Social (E)",
+            "Sr. Ing. Iván Arturo Delgado Quingalombo",
+            "Director Técnico de Operaciones Mineras (E)",
+            "Sr. Mgs. William Fernando Muñoz Ruiz",
+            "Analista 1 - Social",
+            "Sr. Lcdo. Steven Alexander Guaranda Bazurto",
+            "Asistente Administrativo",
+            ""
+        ]
+    },
+    "Oficio Nro. ENAMI-SPGE-2026-0005-OFC": {
+        "tipo": "OFICIO",
+        "id": "Oficio Nro. ENAMI-SPGE-2026-0005-OFC",
+        "fecha": "Quito, D.M., 11 de marzo de 2026",
+        "para": "N/D",
+        "de": "<b>Mgs. Xavier Floresmilo Bolaños Cuzco</b><br><small>SUBGERENTE DE PLANIFICACIÓN Y GESTIÓN ESTRATÉGICA</small>",
+        "asunto": "Respuesta a la solicitud de informe técnico para actividades priorizadas - Programa de Gestión Sostenible e Infraestructura Asociada Señor Magíster Jorge Roberto Solano Mera Coordinador General de Planificación y Gestión Estratégica",
+        "copias": [
+            "Señora Máster",
+            "Ms. Inés María Manzano Díaz",
+            "Ministra de Ambiente y Energía",
+            "MINISTERIO DE AMBIENTE Y ENERGÍA",
+            "Señor Magíster",
+            "Guillermo Iván Flores Caamaño",
+            "Viceministro de Minas",
+            "MINISTERIO DE AMBIENTE Y ENERGÍA",
+            "Señor Magíster",
+            "Galo Andrés García Medina",
+            "Gerente General, Subrogante",
+            "Señor Magíster",
+            "Diego Andrés Rueda Albuja",
+            "Gerente de Gestión y Desarrollo Minero",
+            "Señor Ingeniero",
+            "Francisco Javier Gudiño Lara",
+            "Analista 1 - Procesos",
+            ""
+        ]
+    },
+    "Memorando Nro. ENAMI-SPGE-2026-0033-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-SPGE-2026-0033-MEM",
+        "fecha": "Quito, D.M., 12 de marzo de 2026",
+        "para": "<b>Sr. Mgs. Galo Andrés García Medina</b><br>Gerente General, Subrogante",
+        "de": "<b>Empresa Nacional Minera</b><br><small>Dirección: Av. 6 de Diciembre N31-110 y Whymper, Edificio Torres Tenerife</small>",
+        "asunto": "Solicitud de Aprobación de Modificación POA - Gerencia Gestión y Desarrollo Minero - Dirección Técnica de Operaciones Mineras",
+        "copias": [
+            "Sr. Mgs. Diego Andrés Rueda Albuja",
+            "Gerente de Gestión y Desarrollo Minero",
+            "Sra. Mgs. Viviana del Carmen Almeida Moreno",
+            "Subgerente Administrativa Financiera",
+            "Sr. Ing. Francisco Javier Gudiño Lara",
+            "Analista 1 - Procesos",
+            ""
+        ]
+    },
+    "Memorando Nro. ENAMI-GDM-2026-0060-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-GDM-2026-0060-MEM",
+        "fecha": "Quito, D.M., 13 de marzo de 2026",
+        "para": "<b>Sr. Mgs. Galo Andrés García Medina</b><br>Gerente General, Subrogante",
+        "de": "<b>Mgs. Diego Andrés Rueda Albuja</b><br><small>GERENTE DE GESTIÓN Y DESARROLLO MINERO</small>",
+        "asunto": "Remisión de informes de la comisión de servicios ENAMI-ITC-GDM-DTOM-2026-008",
+        "copias": [
+            "Sra. Soc. Jessica Jacqueline Macas Machuca",
+            "Analista 1 - Social",
+            "Sr. Lcdo. Steven Alexander Guaranda Bazurto",
+            "Asistente Administrativo",
+            ""
+        ]
+    }
+};
