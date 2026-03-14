bd480296e066c935b0be6bfcd946dffebdac2c85
emito182-droid <emito182@gmail.com>
2026-03-14 11:04:42 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,131 @@
+const db_documentos = {
+    "Memorando Nro. ENAMI-GDM-2026-0049-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-GDM-2026-0049-MEM",
+        "fecha": "Quito, D.M., 25 de febrero de 2026",
+        "para": "<b>Sr. Mgs. Xavier Floresmilo Bolaños Cuzco</b><br>Subgerente de Planificación y Gestión Estratégica",
+        "de": "<b>Mgs. Diego Andrés Rueda Albuja</b><br><small>GERENTE DE GESTIÓN Y DESARROLLO MINERO</small>",
+        "asunto": "Respuesta: Remisión de Información - Rendición de Cuentas 2025 GGDM",
+        "copias": [
+            "Sr. Mgs. Galo Andrés García Medina",
+            "<b>Gerente General, Subrogante</b>",
+            "Sr. Ing. Iván Arturo Delgado Quingalombo",
+            "<b>Director Técnico de Operaciones Mineras (E)</b>",
+            "Sr. Ing. Marlon Oswaldo Lara Mier",
+            "<b>Director Ambiental y Social (E)</b>",
+            "Sra. Soc. Jessica Jacqueline Macas Machuca",
+            "<b>Analista 1 - Social</b>",
+            "Sr. Lcdo. Steven Alexander Guaranda Bazurto",
+            "<b>Asistente Adminsitrativo</b>",
+            ""
+        ],
+        "anexos": []
+    },
+    "Memorando Nro. ENAMI-GDM-2026-0057-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-GDM-2026-0057-MEM",
+        "fecha": "Quito, D.M., 10 de marzo de 2026",
+        "para": "<b>Sr. Mgs. Galo Andrés García Medina</b><br>Gerente General, Subrogante",
+        "de": "<b>Mgs. Diego Andrés Rueda Albuja</b><br><small>GERENTE DE GESTIÓN Y DESARROLLO MINERO</small>",
+        "asunto": "Remisión de Informe de Comisión de Servicios ENAMI-ITC-GDM-DTOM-2026-0006",
+        "copias": [
+            "Sra. Soc. Jessica Jacqueline Macas Machuca",
+            "<b>Analista 1 - Social</b>",
+            ""
+        ],
+        "anexos": []
+    },
+    "Memorando Nro. ENAMI-GDM-2026-0059-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-GDM-2026-0059-MEM",
+        "fecha": "Quito, D.M., 11 de marzo de 2026",
+        "para": "<b>Sra. Abg. Gabriela Sibia Jauregui Moran</b><br>Gerente Negocios Mineros",
+        "de": "<b>Mgs. Diego Andrés Rueda Albuja</b><br><small>GERENTE DE GESTIÓN Y DESARROLLO MINERO</small>",
+        "asunto": "Consideración y análisis del Informe Técnico No. 79-DAS-2025 - Visita a los Proyectos de la Empresa SILVERCORP (Diciembre 2025)",
+        "copias": [
+            "Sr. Mgs. Galo Andrés García Medina",
+            "<b>Gerente General, Subrogante</b>",
+            "Sr. Mgs. Lenin Vladimir Ochoa Ochoa",
+            "<b>Subgerente Jurídico</b>",
+            "Sr. Ing. Marlon Oswaldo Lara Mier",
+            "<b>Director Ambiental y Social (E)</b>",
+            "Sr. Ing. Iván Arturo Delgado Quingalombo",
+            "<b>Director Técnico de Operaciones Mineras (E)</b>",
+            "Sr. Mgs. William Fernando Muñoz Ruiz",
+            "<b>Analista 1 - Social</b>",
+            "Sr. Lcdo. Steven Alexander Guaranda Bazurto",
+            "<b>Asistente Administrativo</b>",
+            ""
+        ],
+        "anexos": []
+    },
+    "Memorando Nro. ENAMI-SAF-2026-0062-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-SAF-2026-0062-MEM",
+        "fecha": "Quito, D.M., 04 de marzo de 2026",
+        "para": "<b>Sr. Mgs. Diego Andrés Rueda Albuja</b><br>Gerente de Gestión y Desarrollo Minero",
+        "de": "<b>Mgs. Viviana del Carmen Almeida Moreno</b><br><small>SUBGERENTE ADMINISTRATIVA FINANCIERA</small>",
+        "asunto": "Solicitud de revisión y actualización del Manual de Procedimientos.",
+        "copias": [
+            "Srta. Mgs. Johanna Patricia Gavilanez Gallardo",
+            "<b>Directora Administrativa (E)</b>",
+            ""
+        ],
+        "anexos": []
+    },
+    "Oficio Nro. ENAMI-SPGE-2026-0005-OFC": {
+        "tipo": "OFICIO",
+        "id": "Oficio Nro. ENAMI-SPGE-2026-0005-OFC",
+        "fecha": "Quito, D.M., 11 de marzo de 2026",
+        "para": "<b>Señor Magíster</b><br>Jorge Roberto Solano Mera",
+        "de": "<b>Mgs. Xavier Floresmilo Bolaños Cuzco</b><br><small>SUBGERENTE DE PLANIFICACIÓN Y GESTIÓN ESTRATÉGICA</small>",
+        "asunto": "Respuesta a la solicitud de informe técnico para actividades priorizadas - Programa de Gestión Sostenible e Infraestructura Asociada",
+        "copias": [
+            "Señora Máster",
+            "Ms. Inés María Manzano Díaz",
+            "<b>Ministra de Ambiente y Energía</b>",
+            "MINISTERIO DE AMBIENTE Y ENERGÍA",
+            "Señor Magíster",
+            "Guillermo Iván Flores Caamaño",
+            "<b>Viceministro de Minas</b>",
+            "MINISTERIO DE AMBIENTE Y ENERGÍA",
+            "Señor Magíster",
+            "Galo Andrés García Medina",
+            "<b>Gerente General, Subrogante</b>",
+            "Señor Magíster",
+            "Diego Andrés Rueda Albuja",
+            "<b>Gerente de Gestión y Desarrollo Minero</b>",
+            "Señor Ingeniero",
+            "Francisco Javier Gudiño Lara",
+            "<b>Analista 1 - Procesos</b>",
+            ""
+        ],
+        "anexos": [
+            "implementacion_de_refineria_de_oro_para_enami_ep-signed.pdf",
+            "implementacion_de_planta_de_beneficio_de_minerales_para_la_enami_ep-signed.pdf"
+        ]
+    },
+    "Memorando Nro. ENAMI-SPGE-2026-0033-MEM": {
+        "tipo": "MEMORANDO",
+        "id": "Memorando Nro. ENAMI-SPGE-2026-0033-MEM",
+        "fecha": "Quito, D.M., 12 de marzo de 2026",
+        "para": "<b>Sr. Mgs. Galo Andrés García Medina</b><br>Gerente General, Subrogante",
+        "de": "<b>Mgs. Xavier Floresmilo Bolaños Cuzco</b><br><small>SUBGERENTE DE PLANIFICACIÓN Y GESTIÓN ESTRATÉGICA</small>",
+        "asunto": "Solicitud de Aprobación de Modificación POA - Gerencia Gestión y Desarrollo Minero - Dirección Técnica de Operaciones Mineras",
+        "copias": [
+            "Sr. Mgs. Diego Andrés Rueda Albuja",
+            "<b>Gerente de Gestión y Desarrollo Minero</b>",
+            "Sra. Mgs. Viviana del Carmen Almeida Moreno",
+            "<b>Subgerente Administrativa Financiera</b>",
+            "Sr. Ing. Francisco Javier Gudiño Lara",
+            "<b>Analista 1 - Procesos</b>",
+            ""
+        ],
+        "anexos": [
+            "enami-gdm-2026-0058-mem-pedido-ggdm.pdf",
+            "for-ase-gpl-pla-03-01_viabilidad_modificaciones_poa_ggdm_dtom-signed.pdf",
+            "modificaciones_poa_dtom_febrero-signed.pdf",
+            "poa_2026_vc_005_vi_001_12_03_2026.xls"
+        ]
+    }
+};
